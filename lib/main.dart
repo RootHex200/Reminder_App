ca85abf5b093b1cfb0c3600d217929c016385335
RootHex200 <sabiturrahman60@gmail.com>
2022-12-30 01:30:08 +0600

    app name,icon,theme change add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:work_manager/service/all_service_initilized.dart';
-import 'package:work_manager/view/homepage/home_page.dart';
+import 'package:work_manager/view/splash_screen.dart';
 
 void main() {
   initlization();
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: SplashScreen(),
     );
   }
 }
---
+++ lib/view/splash_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:work_manager/view/homepage/home_page.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>const HomePage()));
+    });
+
+    return Scaffold(
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(child: Container()),
+            GestureDetector(
+              onTap: () {},
+              child: const Image(
+                  height: 100,
+                  width: 100,
+                  image:
+                      AssetImage("assets/images/YR.png")),
+            ),
+            Expanded(child: Container()),
+            Column(
+              children: [
+                const Text(
+                  "Build Version",
+                  style: TextStyle(color: Colors.black54),
+                ),
+                const Text(
+                  "v1.11.1",
+                  style: TextStyle(color: Colors.black54),
+                ),
+                const SizedBox(height: 30),
+                Container(
+                  height: 4,
+                  width: 100,
+                  decoration: BoxDecoration(
+                      color: Colors.grey.withOpacity(0.3),
+                      borderRadius: BorderRadius.circular(4)),
+                ),
+                const SizedBox(height: 20)
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
